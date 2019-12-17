78375d7ab76df6d3fe0d117387ed5f9965c46793
Ryan Yang <ryanqyang@gmail.com>
2019-12-17 15:21:12 -0800

    finished functionality. Changed state to be held locally.

--- react-final/src/components/TodoList/TodoList.js
+++ react-final/src/components/TodoList/TodoList.js
@@ -1,15 +1,33 @@
 import React, {useState} from 'react';
+import Swal from 'sweetalert2';
 import './TodoList.css';
 
-function add(){
-    alert('sdf');
+function TodoItem({content}){
+    const [complete, setComplete] = useState(false);
+    const toggle = () => { setComplete(!complete) }
+    return (
+        <div className="todo-item" onClick={toggle}>
+            <div className={`todo-content ${complete && "todo-content"}`}> {content} </div>
+            <div className="todo-check"> {complete ? "⦿" : "◯"} </div>
+        </div>
+    );
 }
 
 function TodoList(){
     const [todos, setTodos] = useState([]);
 
+    const add = async () => {
+        const { value: text } = await Swal.fire({
+            title: 'Add Todo',
+            input: 'text',
+            inputPlaceholder: 'Enter Name of Todo'
+        });
+        if(text) setTodos([ ...todos, text ])
+    }
+
     return (
         <div id="todo-list">
+            { todos.map( (e, i) => <TodoItem key={i} content={e}/> ) }
             <div className="todo-user--add" onClick={add}>
                 <div className="todo-content todo-content--add"> add an item </div>
             </div>
